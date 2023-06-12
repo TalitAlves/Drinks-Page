fd2b84e390ebeb6a57abb2e32e25691217960f96
Talita Alves <talitagalves91@gmail.com>
2023-06-12 21:59:58 +0200

    add form and new-Drink component

--- Tienda-Drinks/src/app/shared/form/form.component.ts
+++ Tienda-Drinks/src/app/shared/form/form.component.ts
@@ -6,23 +6,41 @@ import {
   Validators,
 } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Drink } from 'src/app/core/models/drinks.model';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent  {
+export class FormComponent implements OnInit {
+  public formDrink?: FormGroup;
+  public newDrinks?: Drink [] = []
 
-  public form?:FormGroup;
+  constructor(private fb: FormBuilder) {}
 
-  constructor(private fb: FormBuilder, private router: Router) {}
-
-  public OnInit():void{
-    this.form = this.fb.group({
-
-      name: new FormControl("" , [Validators.required]),
-      ingridients: new FormControl('', )
-    })
+  public ngOnInit(): void {
+    this.formDrink = this.fb.group({
+      strDrink: new FormControl('', [Validators.required]),
+      strIngredient1: new FormControl('', [Validators.required]),
+      strIngredient2: new FormControl('', [Validators.required]),
+      strIngredient3: new FormControl(''),
+      strIngredient4: new FormControl(''),
+      strIngredient5: new FormControl(''),
+    });
   }
+
+  public creatDrink(){
+    if(this.formDrink?.valid){
+       this.newDrinks?.push({...this.formDrink.value})
+       this.formDrink.reset
+       
+
+    }
+
 }
+
+}
+
+
+
--- Tienda-Drinks/src/app/shared/shared.module.ts
+++ Tienda-Drinks/src/app/shared/shared.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormComponent } from './form/form.component';
 import { RouterModule } from '@angular/router';
-
-
-
-
+import { NewDrinkComponent } from '../pages/new-drink/new-drink.component';
 
 @NgModule({
   declarations: [
     FormComponent,
+    
     
   ],
   imports: [
